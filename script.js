7ae8dddefb65b500cf2d793d71d858516dce69bc
Kartikey Singh <92157475+Kartikey06dk@users.noreply.github.com>
2023-07-26 16:16:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function removeSelectedOption() {
+			const selectElement = document.getElementById("colorSelect");
+			const selectedOption = selectElement.options[selectElement.selectedIndex];
+
+			if (selectedOption) {
+				selectElement.removeChild(selectedOption);
+			}
+		}
